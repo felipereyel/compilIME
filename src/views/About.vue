98a1b29821a70c7dfcb729865347b29317c21422
felipe reyel <felipe.reyel@gmail.com>
2021-08-22 14:33:10 -0300

    add static and about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>Frontend for SSL compiler</h2>
+    <h3>Authors</h3>
+    <lu>
+      <li>Felipe Reyel</li>
+      <li>Leonardo Gomes</li>
+    </lu>
+    <h3>Sources</h3>
+    <lu>
+      <li>
+        Compiler:
+        <a href="https://github.com/felipereyel/ssl-compiler-ime">
+          https://github.com/felipereyel/ssl-compiler-ime
+        </a>
+      </li>
+      <li>
+        Frontend:
+        <a href="https://github.com/felipereyel/compilime">
+          https://github.com/felipereyel/compilime
+        </a>
+      </li>
+    </lu>
   </div>
 </template>
