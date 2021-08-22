e5ade6be4cb8c2aea103531fa584d6b66fdf80b6
felipe reyel <felipe.reyel@gmail.com>
2021-08-22 13:51:28 -0300

    code component

---
+++ src/components/CodeView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="code">
+    <div class="line" v-for="(line, idx) in code.split('\n')" :key="idx">
+      <pre class="index">{{ idx + 1 }}</pre>
+      <pre class="content">{{ line }}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CodeView",
+  props: {
+    code: String,
+  },
+};
+</script>
+
+<style scoped>
+.code {
+  background-color: #e9e9e9;
+  max-height: 300px;
+  overflow: auto;
+}
+
+.line {
+  display: flex;
+  flex-direction: row;
+}
+
+.index {
+  width: 20px;
+  height: 20px;
+  background-color: #adc5a8;
+}
+
+pre {
+  margin: 0;
+  padding: 2px 5px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,7 @@
     <h1>Compilador SSL - IME</h1>
     <input type="file" :multiple="false" @input="fileInput" />
     <div v-if="fileContent" class="compiler">
-      <div class="code">
-        <div
-          class="line"
-          v-for="(line, idx) in fileContent.split('\n')"
-          :key="idx"
-        >
-          <pre class="index">{{ idx + 1 }}</pre>
-          <pre class="content">{{ line }}</pre>
-        </div>
-      </div>
+      <code-view :code="fileContent" />
       <div class="controls">
         <div class="control-buttons">
           <button @click="lexical">Lexical analysis</button>
@@ -20,6 +11,7 @@
           <button disabled>Compile!</button>
         </div>
         <div v-if="analysisLogs.length" class="logs">
+          <h3>Result</h3>
           <span v-for="log in analysisLogs" :key="log" class="log">
             {{ log }}
           </span>
@@ -31,9 +23,13 @@
 
 <script>
 import Lexical from "../utils/compiler/src/lexical";
+import CodeView from "../components/CodeView.vue";
 
 export default {
   name: "Home",
+  components: {
+    CodeView,
+  },
   data() {
     return {
       fileContent: null,
@@ -72,28 +68,6 @@ export default {
 <style scoped>
 .code {
   margin: 15px 0;
-  background-color: #c7c5c5;
-
-  background-color: #e9e9e9;
-
-  max-height: 300px;
-  overflow: auto;
-}
-
-.line {
-  display: flex;
-  flex-direction: row;
-}
-
-.index {
-  width: 20px;
-  height: 20px;
-  background-color: #adc5a8;
-}
-
-pre {
-  margin: 0;
-  padding: 2px 5px;
 }
 
 .control-buttons {
@@ -105,5 +79,9 @@ pre {
 .logs {
   display: flex;
   flex-direction: column;
+}
+
+.log {
+  padding: 5px 0;
 }
 </style>
