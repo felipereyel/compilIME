f5495a003df5ba9de9729cf03f4cfcac59d324a9
felipe reyel <felipe.reyel@gmail.com>
2021-10-15 02:31:30 -0300

    up

--- src/utils/examples.js
+++ src/utils/examples.js
@@ -7,9 +7,19 @@ export const options = [
 `,
   },
   {
-    label: "Algebraic function",
+    label: "Simple Algebraic function",
     value: `function algebra () : integer {
       return (1 + 2) * ( 3 / 4 );
+}
+    `,
+  },
+  {
+    label: "Simple Algebraic Formula",
+    value: `function StaticAvg () : integer {
+      var a, b: integer;
+      a = 2;
+      b = 3;
+        return (a + b) / 2;
 }
     `,
   },
